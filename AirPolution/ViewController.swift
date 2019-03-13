b17cf1033a1518a31a9a7c310772db30aaf3cc7a
RMS2018 <rms2018@MacAir0.local>
2019-03-13 10:53:17 +0100

    Initial Commit

---
+++ AirPolution/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirPolution
+//
+//  Created by RMS2018 on 13/03/2019.
+//  Copyright © 2019 example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
